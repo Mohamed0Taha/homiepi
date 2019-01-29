3dde94b7d580a770b7bcd6d79e8bb744f58f4b67
Mohamed Taha <mohamed.taha@moval.fi>
2019-01-29 22:59:26 +0200

    deploy

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var io = require('socket.io')(server);
 
 server.listen(process.env.PORT || 3000);
 
-/*app.get('/', function(req, res){
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});*/
+});
 
 io.on('connection', function(socket){
     socket.on('on', function(){
